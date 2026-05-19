cea64df0bc86248abdec0fdf2230c5545b39b66c
andrs <andreylucasvdpl2020@gmail.com>
2026-05-18 22:03:44 -0300

    Create beecrowd_1006.c

---
+++ Programação Competitiva/Atividades Elétricas/beecrowd_1006.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+ double ai, bi, ci, amain;
+ 
+ scanf("%lf%lf%lf",&ai,&bi,&ci);
+ 
+ amain = 0>=10;
+ 
+ ai = ai*2;
+ bi = bi*3;
+ ci = ci*5;
+ 
+ amain = (ai + bi + ci)/10;
+ 
+ printf("MEDIA = %.1lf\n", amain);
+
+  return 0;
+}
