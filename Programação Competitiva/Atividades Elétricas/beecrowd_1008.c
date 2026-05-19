ca250f4cd4d7a1d68bdd01b8f023835495a6b64d
andrs <andreylucasvdpl2020@gmail.com>
2026-05-18 22:31:51 -0300

    Update beecrowd_1008.c
    Removed redundant main function and added salary calculation logic.

--- Programação Competitiva/Atividades Elétricas/beecrowd_1008.c
+++ Programação Competitiva/Atividades Elétricas/beecrowd_1008.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+ 
+int main() {
+ int bagui, tempo;
+double pouco, merreca;
+ 
+    scanf("%d%d", &bagui, &tempo);
+ 
+    scanf("%lf", &pouco);
+ 
+    merreca = tempo * pouco;
+ 
+    printf("NUMBER = %d\n", bagui);
+    printf("SALARY = U$ %.2lf\n", merreca);
+ 
+ 
+    return 0;
+}
 
+
+int main(){
+printf("esqueci q tinha q fazer diferente quando trocava o ngc (%), me compliquei atoa\n");
+return 0;}
