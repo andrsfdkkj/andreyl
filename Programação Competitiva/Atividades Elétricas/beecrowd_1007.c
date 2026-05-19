e5d6ac5881b1dd6895a6da664a6020888507391e
andrs <andreylucasvdpl2020@gmail.com>
2026-05-18 22:11:59 -0300

    Update beecrowd_1007.c

--- Programação Competitiva/Atividades Elétricas/beecrowd_1007.c
+++ Programação Competitiva/Atividades Elétricas/beecrowd_1007.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+ 
+int main() {
+ int eu, tu, nos, vos, eles;
+ 
+  scanf("%d%d%d%d",&eu,&tu,&nos,&vos);
+  
+  eles = (eu*tu - nos*vos);
+  
+  printf("DIFERENCA = %d\n",eles);
+  
+ 
+    return 0;
+}
