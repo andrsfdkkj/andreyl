4dfa8c5af88f22e39d0663ca6b0a1f52d4d03b76
andrs <andreylucasvdpl2020@gmail.com>
2026-05-18 21:57:12 -0300

    Create beecrowd_1005.c
    Implement beecrowd_1005.c to calculate weighted average.

---
+++ Programação Competitiva/Atividades Elétricas/beecrowd_1005.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ 
+int main() {
+ double ah, be, MEDIA;
+ 
+ scanf("%lf%lf",&ah,&be);
+ 
+ MEDIA = 0>=10;
+ 
+ ah = ah*3.5;
+ 
+ be = be*7.5;
+ 
+MEDIA = ( ah + be )/11;
+ 
+ printf("MEDIA = %.5lf\n",MEDIA);
+return 0;
+}
+
+
+#include <stdio.h>
+int main(){
+printf("fiquei 6 anos tentando entender como q eu ia deixar a media indo de 0 a 10 vou mentir nao\n");
+return 0;
+}
