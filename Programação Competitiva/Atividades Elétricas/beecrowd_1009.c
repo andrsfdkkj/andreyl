98e4bb3141e04f897ed263cfc5232d5ec48951a9
andrs <andreylucasvdpl2020@gmail.com>
2026-05-19 23:11:51 -0300

    Update beecrowd_1009.c

--- Programação Competitiva/Atividades Elétricas/beecrowd_1009.c
+++ Programação Competitiva/Atividades Elétricas/beecrowd_1009.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+ 
+int main() {
+    char escravo[10];
+    double salario, vendas, merreca;
+    
+    scanf("%s\n", escravo);
+    scanf("%lf\n%lf", &salario, &vendas);
+    
+    merreca = salario + vendas*0.15;
+    
+    printf("TOTAL = R$ %.2lf\n", merreca);
+    
+    return 0;
+}
+
+
 
