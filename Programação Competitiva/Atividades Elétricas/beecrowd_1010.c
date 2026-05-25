54976656145bb93c73b48a8dfc2d70d4c3371333
andrs <andreylucasvdpl2020@gmail.com>
2026-05-24 22:17:17 -0300

    Update beecrowd_1010.c

--- Programação Competitiva/Atividades Elétricas/beecrowd_1010.c
+++ Programação Competitiva/Atividades Elétricas/beecrowd_1010.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+    int cdpcum, nmpcum, cdpcdois, nmpcdois;
+    double vlpcum, vlpcdois, valor;
+    
+    scanf("%d%d%lf\n", &cdpcum, &nmpcum, &vlpcum);
+    scanf("%d%d%lf", &cdpcdois, &nmpcdois, &vlpcdois);
+
+    valor = (nmpcum * vlpcum) + (nmpcdois * vlpcdois);
+ 
+    printf("VALOR A PAGAR: R$ %.2lf\n", valor);
+ 
+    return 0;
+}
 
