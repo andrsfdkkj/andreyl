58abd6127ca2de45a0d9900fb0ad4c4c6d9c3713
andrs <andreylucasvdpl2020@gmail.com>
2026-06-14 22:00:57 -0300

    Fix calculation of days in beecrowd_1020.c

--- Programação Competitiva/Atividades Elétricas/beecrowd_1020.c
+++ Programação Competitiva/Atividades Elétricas/beecrowd_1020.c
@@ -8,7 +8,7 @@ int main() {
     anos = diandreys/365;
     diandreys = diandreys % 365;
     mes = diandreys/30;
-    dias = diandreys % 30;
+    diandreys = diandreys % 30;
     
     printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", anos, mes, diandreys);
  
