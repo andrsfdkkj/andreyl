d9215e49d3815fdb8c7cb330121bdb42b5b5fb13
andrs <andreylucasvdpl2020@gmail.com>
2026-08-20 11:39:37 -0300

    Add beecrowd_3302.c for competitive programming

---
+++ Programação Competitiva/Atividades Elétricas/beecrowd_3302.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main(){
+       
+    int N, n;
+    scanf("%d", &N);
+    
+    for(int i=1; i <=  N; i++){
+        scanf("%d", &n);
+        printf("respota %d: %d\n", i, n);
+ 
+ 
+ 
+    }
+    
+    return 0;
+    
+}
