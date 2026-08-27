a6f1f0996669c36b317e462eb9edb1e7cd9790f9
andrs <andreylucasvdpl2020@gmail.com>
2026-08-27 11:17:46 -0300

    Create RES-Beecrowd_1585.c

---
+++ Programação Competitiva/Atividades Beecrowd/Aula_05/RES-Beecrowd_1585.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int N, x, y, area;
+    
+    scanf("%d", &N);
+
+    for(int i = 0; i < N; i++){
+        
+        scanf("%d%d", &x, &y);
+
+        area = (x * y) / 2;
+
+        printf("%d cm2\n", area);}
+
+    return 0;
+}
