0f01cb7a8544c763b1b978df68e631df3edd9212
andrs <andreylucasvdpl2020@gmail.com>
2026-08-27 11:19:47 -0300

    Create RES-Beecrowd_1002.c

---
+++ Programação Competitiva/Atividades Beecrowd/Aula_01/RES-Beecrowd_1002.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+double andrey, an, dr, ey;
+ 
+    scanf("%lf",&dr);
+ 
+andrey = 3.14159;
+
+ey = dr*dr;
+
+an = andrey*ey;
+ 
+    printf("A=%.4lf\n",an);
+  
+  return 0;
+}
